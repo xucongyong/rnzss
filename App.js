f08d930aaf86a40c9a92b6d0ce4bd20602d7c28d
vanxv <ironvanxv@gmail.com>
2018-07-31 18:12:13 +0800

    createReactClass

--- App.js
+++ App.js
@@ -3,12 +3,17 @@ import React, { Component } from 'react';
 import { AppRegistry, StyleSheet, Button, Text, View } from 'react-native';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 
+//** --- input class --- **//
+var Home = require('./component/Home/Home');
+var Main = require('./component/Main/Main');
+
 class HomeScreen extends React.Component {
   render() {
     return (
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
         <Text>Home!</Text>
       </View>
+      // <Home />
     );
   }
 }
@@ -32,38 +37,46 @@ class TextScreen extends React.Component {
   }
 }
 
-export default TabNavigator(
-  {
-    Home: { screen: HomeScreen },
-    Settings: { screen: SettingsScreen },
-    Text: { screen: TextScreen },
-  },
-  {
-    navigationOptions: ({ navigation }) => ({
-      tabBarIcon: ({ focused, tintColor }) => {
-        const { routeName } = navigation.state;
-        let iconName;
-        if (routeName === 'Home') {
-          iconName = `ios-information-circle${focused ? '' : '-outline'}`;
-        } else if (routeName === 'Settings') {
-          iconName = `ios-options${focused ? '' : '-outline'}`;
-        }
-        else if (routeName === 'Text') {
-          iconName = `ios-options${focused ? '' : '-outline'}`;
-        }
-
-        // You can return any component that you like here! We usually use an
-        // icon component from react-native-vector-icons
-        return <Ionicons name={iconName} size={25} color={tintColor} />;
-      },
-    }),
-    tabBarComponent: TabBarBottom,
-    tabBarPosition: 'bottom',
-    tabBarOptions: {
-      activeTintColor: 'tomato',
-      inactiveTintColor: 'gray',
-    },
-    animationEnabled: false,
-    swipeEnabled: false,
+export default class App extends Component<Props> {
+  render() {
+    return (
+      <Main />
+      )
   }
-);
+}
+
+// export default TabNavigator(
+//   {
+//     Home: { screen: HomeScreen },
+//     Settings: { screen: SettingsScreen },
+//     Text: { screen: TextScreen },
+//   },
+//   {
+//     navigationOptions: ({ navigation }) => ({
+//       tabBarIcon: ({ focused, tintColor }) => {
+//         const { routeName } = navigation.state;
+//         let iconName;
+//         if (routeName === 'Home') {
+//           iconName = `ios-information-circle${focused ? '' : '-outline'}`;
+//         } else if (routeName === 'Settings') {
+//           iconName = `ios-options${focused ? '' : '-outline'}`;
+//         }
+//         else if (routeName === 'Text') {
+//           iconName = `ios-options${focused ? '' : '-outline'}`;
+//         }
+
+//         // You can return any component that you like here! We usually use an
+//         // icon component from react-native-vector-icons
+//         return <Ionicons name={iconName} size={25} color={tintColor} />;
+//       },
+//     }),
+//     tabBarComponent: TabBarBottom,
+//     tabBarPosition: 'bottom',
+//     tabBarOptions: {
+//       activeTintColor: 'tomato',
+//       inactiveTintColor: 'gray',
+//     },
+//     animationEnabled: false,
+//     swipeEnabled: false,
+//   }
+// );
