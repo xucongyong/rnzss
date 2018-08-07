cce79cb33e8bc284c71721034e1f2969295898e1
vanxv <ironvanxv@gmail.com>
2018-08-08 00:12:32 +0800

    navigator

--- component/Home/Home.js
+++ component/Home/Home.js
@@ -1,23 +1,53 @@
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-import createReactClass from 'create-react-class'
+import React, { Component } from 'react';
+import {Platform, StyleSheet, Text, View, Button, TextInput} from 'react-native';
+import createReactClass from 'create-react-class';
+import { createStackNavigator } from 'react-navigation';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
 
-type Props = {};
+class HomeScreen extends React.Component{
+    static navigationOptions =({
+        headerTitle: <TextInput></TextInput>
+    })
+  render(){
+
+    return(
+        <View style={{flex:1}}>
+        <Text>xxxx</Text>
+          <Button
+          title = 'HELLO'
+          onPress ={()=> this.props.navigation.navigate('Details')}
+          ></Button>
+        </View>
+    )
+  }
+}
+
+class DeatilScreen extends React.Component{
+    render(){
+        return(
+            <View style={{flex:1}}>
+                <Text>Deatil</Text>
+            </View>
+        )}}
+
+const Screen = createStackNavigator(
+    {
+        Home: {
+          screen: HomeScreen
+        },
+        Details: {
+          screen: DeatilScreen
+        }
+    },
+{
+  initialRouteName: 'Home'
+}
+)
 
 var Home = createReactClass({
 	render() {
       return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to Home!</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
+      <Screen />
     );
   }
 })
