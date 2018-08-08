b9dc3f20d6ac5d68b399aaac6102c11cb938d522
vanxv <ironvanxv@gmail.com>
2018-08-08 16:45:05 +0800

    Top navigator

--- component/Home/Home.js
+++ component/Home/Home.js
@@ -4,9 +4,46 @@ import createReactClass from 'create-react-class';
 import { createStackNavigator } from 'react-navigation';
 
 
+class Hometitle extends React.Component{
+    render(){
+        return(
+            <View style={{flex:1, backgroundColor:'white'}}>
+                <TextInput
+                    placeholder="  商家、产品名称、商圈"
+
+                ></TextInput>
+            </View>
+        )}}
+class HomeHeaderLeft extends React.Component{
+    render(){
+        return(
+            <View style={{flex:1, flexDirection:'row',}}>
+                <Button style={{width:2, height:40,}}
+                    title='城市'
+                ></Button>
+
+            </View>
+        )}}
+class HomeHeaderRight extends React.Component{
+    render(){
+        return(
+            <View style={{flex:1}}>
+                <Button
+                    title='搜索'
+                ></Button>
+            </View>
+        )}}
+
+
 class HomeScreen extends React.Component{
     static navigationOptions =({
-        headerTitle: <TextInput></TextInput>
+        headerTitle: <Hometitle/>, //<TextInput></TextInput>,
+        headerLeft: <HomeHeaderLeft />,
+        headerRight: <HomeHeaderRight />,
+
+        headerStyle: {
+            backgroundColor: '#f4511e',
+        }
     })
   render(){
 
@@ -41,7 +78,7 @@ const Screen = createStackNavigator(
     },
 {
   initialRouteName: 'Home'
-}
+},
 )
 
 var Home = createReactClass({
@@ -55,7 +92,6 @@ var Home = createReactClass({
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
@@ -70,6 +106,9 @@ const styles = StyleSheet.create({
     color: '#333333',
     marginBottom: 5,
   },
+    Headertitle: {
+
+    }
 });
 
 // output class
