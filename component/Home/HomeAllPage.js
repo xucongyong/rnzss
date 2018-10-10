4f7d860fd642a8d54512e8aad548011f922dc3f5
vanxv <ironvanxv@gmail.com>
2018-10-10 16:55:10 +0800

    productlistview

--- component/Home/HomeAllPage.js
+++ component/Home/HomeAllPage.js
@@ -23,86 +23,64 @@ let token = ''
 export default class MyComponent extends React.Component {
     constructor(props){
         super(props);
-        const ds = new ListView.DataSource({rowHasChanged : (row1, row2) =>  row1 !== row2});
+        const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
         this.state = {
             dataSource : ds,
-            isLoading:false,
-            refreshing:false,
+            isLoading:false, //载入数据判断
+            refreshing:false, //
             isMoreloading:true,
         }
     }
-
     componentDidMount(){
-        // 菊花加载
-        console.log('component Will Mount')
+        // 加载图片
         this.setState({
             isLoading:true,
         })
-
-
-        this.fetchData();
+        this.fetchData(); //启动的时候载入数据
     }
     fetchData(refresh){
         if(refresh){
             this.setState({
                 refreshing:true
             });
-            }
-        // device.DeviceID = DeviceInfo.getUniqueID();
+        }
         deviceStorage.get('token').then((GetToken) => {
-                token = GetToken
-                console.log(token=== '')
-                console.log(token=== null)
-                
-                axios.get(MyUrl, { headers: { Authorization: token, sort:0,version:'1.0'}})
-                    .then(response => {
-                    // If request is good...
-                        console.log('response.data');
-                        console.log(response.data);
-                        console.log('response.data');
+            token = GetToken
+            console.log(token=== '')
+            console.log(token=== null)
+            axios.get(MyUrl, { headers: { Authorization: token, sort:0,version:'1.0'}})
+                .then(response => {
+                    console.log(response.data)
+                    console.log(typeof response.data)
+                    console.log(typeof response.data)
+                    this.setState({
+                        dataSource: this.state.dataSource.cloneWithRows(response.data),
+                        isLoading:false,
+                        refreshing:false,
                     })
-                    .catch((error) => {
-                        console.log('error 3 ' + error);
-                    });
+                })
+                .catch((error) => {
+                    console.log('error 3 ' + error);
                 });
-        // Make a request for a user with a given ID
-
-        // fetch(MyUrl)
-        //     .then((response) => response.json())
-        //     .then((data) => {
-        //         let dataList = data.subjects;
-        //         this.setState({
-        //             dataSource:this.state.dataSource.cloneWithRows(
-        //                 ),
-        //             isLoading:false,
-        //             refreshing:false
-        //         })
-        //     })
-        //     .catch((err) => {
-        //         console.log(err)
-        //     })
-        //     .done()
+        });
     }
-
+    //listview数据加工成页面
     _renderRow(data){
+        console.log('data')
+        console.log(data['Details'])
+        var testV  = JSON.parse(data['Details'])
+        console.log(typeof testV)
+        console.log(testV)
+        console.log(typeof data['Details'])
         return (
             <TouchableOpacity onPress={()=> {
-                this.props.navigation.navigate('ProductScreen',{data:data})
-            }}>
+                this.props.navigation.navigate('ProductScreen',{data:data['Id']})}}>
             <View style={styles.cellBoxStyle}>
-
-                <Image source={{uri:data.images.large}} style={{width:70,height:70}} />
-                    <View>
-                        <Text style={styles.cellTxt}>{data.title}</Text>
-                        <Text style={styles.cellTxt}>{data.genres}</Text>
-                    </View>
-                    <View>
-                        <Text style={styles.cellTxt}>礼物</Text>
-                        <Text style={styles.cellTxt}>红包</Text>
-                        <Text style={styles.cellTxt}>报名</Text>
-                        <Text style={styles.cellTxt}>返现</Text>
-                    </View>
+                <View>
+                    <Text style={styles.cellTxt}>{data['Id']}</Text>
+                    <Text style={styles.cellTxt}>{testV['name']}</Text>
                 </View>
+            </View>
             </TouchableOpacity>
 
         )
@@ -127,41 +105,32 @@ export default class MyComponent extends React.Component {
                 </View>
             )
         }
-
     }
-
     // 滑动到做底下的时候
     _toEnd(){
-
         setTimeout(() => {
             this.setState({
                 isMoreloading:true
             })
         },2000)
     }
-
     render() {
-
         let viewList;
         if(this.state.isLoading){
             viewList = (
-                <ActivityIndicator
-                    size="large"
-                    color='red'
-                    style={{marginTop:50}}
-                />
+                <ActivityIndicator color="#0000ff" style={{marginTop:50}} />
             )
         }else{
             viewList = (
                 <ListView
                     dataSource={this.state.dataSource}
                     renderRow={(data) => this._renderRow(data)}
-                    refreshControl={
-                        <RefreshControl
-                            refreshing={this.state.refreshing}
-                            onRefresh={this.reloadNewData.bind(this)}
-                            colors={['red','orange']}
-                        />}
+                    // refreshControl={
+                    //     <RefreshControl
+                    //         refreshing={this.state.refreshing}
+                    //         onRefresh={this.reloadNewData.bind(this)}
+                    //         colors={['red','orange']}
+                    //     />}
                     renderFooter={()=>this.renderFooter()}
                     onEndReached={ ()=>this._toEnd() }
                 />
@@ -172,37 +141,8 @@ export default class MyComponent extends React.Component {
             <View style={styles.container}>
                 {/*练习下拉刷新，上拉加载组件，此处渲染视图*/}
                 {viewList}
-
-                {/*<View style={styles.shopcart}>*/}
-                    {/*<View style={{flex: 2, flexDirection: 'row'}}>*/}
-                        {/*<View style={styles.bottomItem}>*/}
-                            {/*<Text>客服</Text>*/}
-                        {/*</View>*/}
-                        {/*<View style={styles.bottomItem}>*/}
-                            {/*<Text>后仓</Text>*/}
-                        {/*</View>*/}
-
-                        {/*<View style={styles.bottomItem}>*/}
-
-                            {/*<Text>购物车</Text>*/}
-
-                        {/*</View>*/}
-
-                    {/*</View>*/}
-
-                    {/*<View style={[styles.bottomItem, {backgroundColor: 'red'}]}>*/}
-
-                        {/*<Text>加入购物车</Text>*/}
-                    {/*</View>*/}
-                    {/*<View style={[styles.bottomItem, {backgroundColor: 'green'}]}>*/}
-                        {/*<Text>看左面{'\n'}加入{'\n'}购物车</Text>*/}
-                    {/*</View>*/}
-
-                {/*</View>*/}
             </View>
-
         )
-
     }
 }
 
@@ -239,7 +179,7 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
         borderWidth: 1
-        },
+    },
     shopcart: {
         position: 'absolute',
         bottom: 0,
