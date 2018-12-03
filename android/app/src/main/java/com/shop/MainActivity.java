1dd89f112dca402c5ff88056ee108be7d313497a
vanxv <vanxv@vandeMacBook-Pro.local>
2018-12-03 12:57:58 +0800

    umeng 50% start safe RNinfo

--- android/app/src/main/java/com/shop/MainActivity.java
+++ android/app/src/main/java/com/shop/MainActivity.java
@@ -1,6 +1,7 @@
 package com.shop;
 
 import com.facebook.react.ReactActivity;
+import com.umeng.analytics.MobclickAgent;
 
 public class MainActivity extends ReactActivity {
 
@@ -8,6 +9,16 @@ public class MainActivity extends ReactActivity {
      * Returns the name of the main component registered from JavaScript.
      * This is used to schedule rendering of the component.
      */
+    public void onResume() {
+        super.onResume();
+        MobclickAgent.onResume(this);
+    }
+    public void onPause() {
+        super.onPause();
+        MobclickAgent.onPause(this);
+    }
+
+
     @Override
     protected String getMainComponentName() {
         return "shop";
